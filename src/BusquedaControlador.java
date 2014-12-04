abb9bddcf3aa913211562101d9f38c98653513db
MRUIZGA <MRUIZGA@CC314.itam.mx>
2014-12-04 17:51:14 -0600

    Importantes mejoras.

--- src/BusquedaControlador.java
+++ src/BusquedaControlador.java
@@ -5,105 +5,112 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 
 public class BusquedaControlador extends BusquedaVista {
-	//private int indiceCarta = 0;
-	protected String datosPrueba[] = {"Summon skull", "Giant Rat", "Hane-hane", "Dark hole", "Soul charge","Kuribo","Dark Bribe"};
+	// private int indiceCarta = 0;
+	protected String datosPrueba[] = { "Summon skull", "Giant Rat",
+			"Hane-hane", "Dark hole", "Soul charge", "Kuribo", "Dark Bribe" };
 	protected int indiceCarta = 0;
-	//public static String indicaExp;
-	private int ind1=0,ind2=1,ind3=2;
+	// public static String indicaExp;
+	private int ind1 = 0, ind2 = 1, ind3 = 2;
 
-	public BusquedaControlador(){
+	public BusquedaControlador() {
 		super();
 		construye();
-		while (indiceCarta < MAXCARTAS && indiceCarta < datosPrueba.length){
+		while (indiceCarta < MAXCARTAS && indiceCarta < datosPrueba.length) {
 			btnsCartas[indiceCarta].setText(datosPrueba[indiceCarta]);
 			indiceCarta++;
 		}
 	}
 
-	
-	public BusquedaControlador(ListadoCartas listadoCartas){
+	public BusquedaControlador(ListadoCartas listadoCartas) {
 		super();
 		construye();
-		while (indiceCarta < MAXCARTAS && indiceCarta < listadoCartas.getNumCartas()){
-			btnsCartas[indiceCarta].setText(listadoCartas.getNombreCarta(indiceCarta));
+		while (indiceCarta < MAXCARTAS
+				&& indiceCarta < listadoCartas.getNumCartas()) {
+			btnsCartas[indiceCarta].setText(listadoCartas
+					.getNombreCarta(indiceCarta));
 			indiceCarta++;
 		}
 	}
-	
-	private void construye(){
+
+	private void construye() {
 		regresar.addActionListener(new Escuchador());
 		btnArriba.addActionListener(new Escuchador());
 		btnAbajo.addActionListener(new Escuchador());
-	    btnsCartas[0].addActionListener(new EscuchadorB1());
-	    btnsCartas[1].addActionListener(new EscuchadorB2());
-	    btnsCartas[2].addActionListener(new EscuchadorB3());
+		btnsCartas[0].addActionListener(new EscuchadorB1());
+		btnsCartas[1].addActionListener(new EscuchadorB2());
+		btnsCartas[2].addActionListener(new EscuchadorB3());
 	}
-		
-	private class EscuchadorB1 implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			if(btnsCartas[0].getText() != "---"){
-				CartaControlador self=new CartaControlador(btnsCartas[0].getText(),ind1);
+
+	private class EscuchadorB1 implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (btnsCartas[0].getText() != "---") {
+				CartaControlador self = new CartaControlador(
+						btnsCartas[0].getText(), ind1);
 			}
 		}
 	}
-	
-	private class EscuchadorB2 implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			if(btnsCartas[1].getText() != "---"){
-				CartaControlador self=new CartaControlador(btnsCartas[1].getText(),ind2);
-			}		
+
+	private class EscuchadorB2 implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (btnsCartas[1].getText() != "---") {
+				CartaControlador self = new CartaControlador(
+						btnsCartas[1].getText(), ind2);
+			}
 		}
 	}
-	
-	private class EscuchadorB3 implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			if(btnsCartas[2].getText() != "---"){
-				CartaControlador self=new CartaControlador(btnsCartas[2].getText(),ind3);
-			}	
+
+	private class EscuchadorB3 implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (btnsCartas[2].getText() != "---") {
+				CartaControlador self = new CartaControlador(
+						btnsCartas[2].getText(), ind3);
+			}
 		}
 	}
-	
+
 	private class Escuchador implements ActionListener {
 		public void actionPerformed(ActionEvent ae) {
-			
+
 			if (ae.getActionCommand().equals("Regresar")) {
 				dispose();
-				InicioControlador self=new InicioControlador();
+				InicioControlador self = new InicioControlador();
 			}
-			if(ae.getActionCommand().equals("▲")){
-				int k=0,j=indiceCarta-(MAXCARTAS-1);
-				
-				if(indiceCarta >= datosPrueba.length){
-					
-				}
-				else{
-					ind1=ind1+1;
-					ind2=ind2+1;
-					ind3=ind3+1;
-					while (k < MAXCARTAS && indiceCarta < InicioControlador.listadoCartas.getNumCartas()){
-						btnsCartas[k].setText(InicioControlador.listadoCartas.getNombreCarta(j));
+			if (ae.getActionCommand().equals("▲")) {
+				int k = 0, j = indiceCarta - (MAXCARTAS - 1);
+
+				if (indiceCarta >= datosPrueba.length) {
+
+				} else {
+					ind1 = ind1 + 1;
+					ind2 = ind2 + 1;
+					ind3 = ind3 + 1;
+					while (k < MAXCARTAS
+							&& indiceCarta < InicioControlador.listadoCartas
+									.getNumCartas()) {
+						btnsCartas[k].setText(InicioControlador.listadoCartas
+								.getNombreCarta(j));
 						j++;
 						k++;
 					}
 					indiceCarta++;
 				}
-				//System.out.println("j: "+j);
-				//System.out.println("indice: "+indiceCarta);
+				// System.out.println("j: "+j);
+				// System.out.println("indice: "+indiceCarta);
 			}
-			if(ae.getActionCommand().equals("▼")){
-				int k=0,j=indiceCarta-(MAXCARTAS+1);
-				
-				if(j < 0){
-					
-				}
-				else{
-					//System.out.println("j: "+j);
-					//System.out.println("indice: "+indiceCarta);
-					ind1=ind1-1;
-					ind2=ind2-1;
-					ind3=ind3-1;
-					while (k < MAXCARTAS && indiceCarta-(MAXCARTAS+1) >= 0){
-						btnsCartas[k].setText(InicioControlador.listadoCartas.getNombreCarta(j));
+			if (ae.getActionCommand().equals("▼")) {
+				int k = 0, j = indiceCarta - (MAXCARTAS + 1);
+
+				if (j < 0) {
+
+				} else {
+					// System.out.println("j: "+j);
+					// System.out.println("indice: "+indiceCarta);
+					ind1 = ind1 - 1;
+					ind2 = ind2 - 1;
+					ind3 = ind3 - 1;
+					while (k < MAXCARTAS && indiceCarta - (MAXCARTAS + 1) >= 0) {
+						btnsCartas[k].setText(InicioControlador.listadoCartas
+								.getNombreCarta(j));
 						j++;
 						k++;
 					}
@@ -112,8 +119,8 @@ public class BusquedaControlador extends BusquedaVista {
 			}
 		}
 	}
-	
-	public static void main(String [] args){
-			BusquedaControlador self=new BusquedaControlador();
-		}
+
+	public static void main(String[] args) {
+		BusquedaControlador self = new BusquedaControlador();
 	}
+}
